6d55eff4e8849bb4175cbc895872c84e9f209af1
GorkyAnge <92492732+GorkyAnge@users.noreply.github.com>
2023-06-04 13:07:03 -0500

    Update Ejercicio 18.py

--- Seccion 8/Ejercicio 18.py
+++ Seccion 8/Ejercicio 18.py
@@ -1,19 +1,47 @@
 class Node:
     def __init__(self, value):
-        # Store node value
         self.value = value
-        # Reference to next node
         self.next = None
-        # Reference to previous node
         self.prev = None
-        
-class DoublyLinkedList:
+
+class Stack:
     def __init__(self, value):
-        # Create new node with value
         new_node = Node(value)
-        # Set head to new node
-        self.head = new_node
-        # Set tail to new node
-        self.tail = new_node
-        # Set initial length to 1
-        self.length = 1
+        self.top = new_node
+        self.height = 1
+
+    def push(self, value):
+        new_node = Node(value)
+        new_node.prev = self.top
+        self.top.next = new_node
+        self.top = new_node
+        self.height += 1
+
+    def pop(self):
+        if self.height == 0:
+            raise IndexError("Stack is empty")
+        value = self.top.value
+        self.top = self.top.prev
+        if self.top:
+            self.top.next = None
+        self.height -= 1
+        return value
+
+    def is_empty(self):
+        return self.height == 0
+
+
+my_stack = Stack(4)
+
+print('Top:', my_stack.top.value)
+print('Height:', my_stack.height)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Top: 4
+    Height: 1
+
+"""
