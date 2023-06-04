d67f1ece26e50ee463687c4a949485d34427d6d4
GorkyAnge <92492732+GorkyAnge@users.noreply.github.com>
2023-06-04 13:08:56 -0500

    Update Ejercicio 18.py

--- Seccion 8/Ejercicio 18.py
+++ Seccion 8/Ejercicio 18.py
@@ -4,44 +4,15 @@ class Node:
         self.next = None
         self.prev = None
 
-class Stack:
+class DoublyLinkedList:
     def __init__(self, value):
         new_node = Node(value)
-        self.top = new_node
-        self.height = 1
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
 
-    def push(self, value):
-        new_node = Node(value)
-        new_node.prev = self.top
-        self.top.next = new_node
-        self.top = new_node
-        self.height += 1
+my_doubly_linked_list = DoublyLinkedList(7)
 
-    def pop(self):
-        if self.height == 0:
-            raise IndexError("Stack is empty")
-        value = self.top.value
-        self.top = self.top.prev
-        if self.top:
-            self.top.next = None
-        self.height -= 1
-        return value
-
-    def is_empty(self):
-        return self.height == 0
-
-
-my_stack = Stack(4)
-
-print('Top:', my_stack.top.value)
-print('Height:', my_stack.height)
-
-
-
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    Top: 4
-    Height: 1
-
-"""
+print('Head:', my_doubly_linked_list.head.value)
+print('Tail:', my_doubly_linked_list.tail.value)
+print('Length:', my_doubly_linked_list.length)
